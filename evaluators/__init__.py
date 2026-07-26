09a7344e0fbe520f40fa28aacb511bedf4481ec0
rimraf-adi <kinjawadekaradi112@gmail.com>
2026-07-26 19:21:16 +0530

    Add marathi_konkani evaluation script and baseline benchmark YAMLs

--- evaluators/__init__.py
+++ evaluators/__init__.py
@@ -9,6 +9,7 @@ from src.dialect_norm.evaluators.hindi import evaluate_hindi
 from src.dialect_norm.evaluators.kannada import evaluate_kannada
 from src.dialect_norm.evaluators.magahi import evaluate_magahi
 from src.dialect_norm.evaluators.marathi import evaluate_marathi
+from src.dialect_norm.evaluators.marathi_konkani import evaluate_marathi_konkani
 from src.dialect_norm.evaluators.maithili import evaluate_maithili
 from src.dialect_norm.evaluators.telugu import evaluate_telugu
 
@@ -20,6 +21,7 @@ __all__ = [
     "evaluate_kannada",
     "evaluate_magahi",
     "evaluate_marathi",
+    "evaluate_marathi_konkani",
     "evaluate_maithili",
     "evaluate_telugu",
 ]
--- src/dialect_norm/evaluators/__init__.py
+++ src/dialect_norm/evaluators/__init__.py
@@ -1,3 +1,27 @@
 """
 Language-specific evaluation modules.
 """
+
+from .bhojpuri import evaluate_bhojpuri
+from .bengali import evaluate_bengali
+from .chhattisgarhi import evaluate_chhattisgarhi
+from .hindi import evaluate_hindi
+from .kannada import evaluate_kannada
+from .magahi import evaluate_magahi
+from .marathi import evaluate_marathi
+from .marathi_konkani import evaluate_marathi_konkani
+from .maithili import evaluate_maithili
+from .telugu import evaluate_telugu
+
+__all__ = [
+    "evaluate_bhojpuri",
+    "evaluate_bengali",
+    "evaluate_chhattisgarhi",
+    "evaluate_hindi",
+    "evaluate_kannada",
+    "evaluate_magahi",
+    "evaluate_marathi",
+    "evaluate_marathi_konkani",
+    "evaluate_maithili",
+    "evaluate_telugu",
+]
